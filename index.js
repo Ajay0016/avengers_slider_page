576d3db97288ac83c854e205a9231ea86d0c888e
Ajay Sharma <97273806+Ajay0016@users.noreply.github.com>
2024-04-23 14:47:40 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const links = document.querySelectorAll('.navItem');
+const sections = document.querySelectorAll('.section');
+
+function changeLinkState() {
+  let index = sections.length;
+
+  while(--index && window.scrollY + 50 < sections[index].offsetTop) {}
+  
+  links.forEach((link) => link.classList.remove('active'));
+  links[index].classList.add('active');
+
+}
+
+
+
+changeLinkState();
+window.addEventListener('scroll', changeLinkState);
+
